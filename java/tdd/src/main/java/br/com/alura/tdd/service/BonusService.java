f733bdaaf6a8bd61fa2240cd9fb5599506d51d8c
Luiz Rodrigues <luiz.gmr.dev@gmail.com>
2023-06-02 09:28:18 -0300

    Test com Exception

--- java/tdd/src/main/java/br/com/alura/tdd/service/BonusService.java
+++ java/tdd/src/main/java/br/com/alura/tdd/service/BonusService.java
@@ -9,9 +9,11 @@ public class BonusService {
 
     public BigDecimal calcularBonus(Funcionario funcionario) {
         var valor = funcionario.getSalario().multiply(BigDecimal.valueOf(0.1));
+
         if (valor.compareTo(Utils.novoBigDecimalEscalaDuasCasasDecimais(1000)) > 0) {
-            valor = Utils.escalaDuasCasasDecimais(BigDecimal.ZERO);
+            throw new IllegalArgumentException("Funcionário com salário maior do que R$10.000,00 não pode receber bônus.");
         }
+
         return Utils.escalaDuasCasasDecimais(valor);
     }
 
--- java/tdd/src/test/java/service/BonusServiceTest.java
+++ java/tdd/src/test/java/service/BonusServiceTest.java
@@ -6,16 +6,21 @@ import br.com.alura.tdd.utils.Utils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 
 class BonusServiceTest {
     @Test
     void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto() {
         final var service = new BonusService();
-        final var bonus = service.calcularBonus(new Funcionario("Jéssica", LocalDate.now(), Utils.novoBigDecimalEscalaDuasCasasDecimais(25000)));
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> service.calcularBonus(new Funcionario("Jéssica", LocalDate.now(), Utils.novoBigDecimalEscalaDuasCasasDecimais(25000))));
 
-        Assertions.assertEquals(Utils.escalaDuasCasasDecimais(BigDecimal.ZERO), bonus);
+        /*try {
+            service.calcularBonus(new Funcionario("Jéssica", LocalDate.now(), Utils.novoBigDecimalEscalaDuasCasasDecimais(25000)));
+            Assertions.fail("Não deu a Exception.");
+        } catch (Exception e) {
+            Assertions.assertEquals("Funcionário com salário maior do que R$10.000,00 não pode receber bônus.", e.getMessage());
+        }*/
     }
 
     @Test
